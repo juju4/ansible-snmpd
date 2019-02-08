25152778674ed50e25f85cce6e5a25238e64c778
juju4 <juju4@users.noreply.github.com>
2019-02-07 20:37:47 -0500

    galaxy lint

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -9,7 +9,7 @@ else
 rolesdir=$(dirname $0)/..
 fi
 
-#[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
+#[ ! -d $rolesdir/juju4.redhat_epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat_epel
 ## galaxy naming: kitchen fails to transfer symlink folder
 #[ ! -e $rolesdir/juju4.snmpd ] && ln -s ansible-snmpd $rolesdir/juju4.snmpd
 [ ! -e $rolesdir/juju4.snmpd ] && cp -R $rolesdir/ansible-snmpd $rolesdir/juju4.snmpd
